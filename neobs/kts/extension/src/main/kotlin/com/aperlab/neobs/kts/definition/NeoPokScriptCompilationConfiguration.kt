bf1020dd4624c49b0e175113871eb69937f20a1e
dpeter99 <dpeter99@gmail.com>
2024-01-06 14:34:28 +0100

    Fix folder naming

---
+++ neobs/kts/extension/src/main/kotlin/com/aperlab/neobs/kts/definition/NeoPokScriptCompilationConfiguration.kt
@@ -0,0 +1,29 @@
+package com.aperlab.neobs.kts.definition
+
+import kotlinx.coroutines.runBlocking
+import kotlin.script.experimental.api.*
+import kotlin.script.experimental.dependencies.*
+import kotlin.script.experimental.dependencies.maven.MavenDependenciesResolver
+import kotlin.script.experimental.jvm.JvmDependency
+import kotlin.script.experimental.jvm.dependenciesFromCurrentContext
+import kotlin.script.experimental.jvm.jvm
+
+object neobsScriptCompilationConfiguration : ScriptCompilationConfiguration(
+        {
+            // Implicit imports for all scripts of this type
+            defaultImports("com.aperlab.neobs.kts.definition.*")
+            jvm {
+                // Extract the whole classpath from context classloader and use it as dependencies
+                dependenciesFromCurrentContext(wholeClasspath = true)
+            }
+            // Callbacks
+            refineConfiguration {
+                // Process specified annotations with the provided handler
+                //onAnnotations(DependsOn::class, Repository::class, handler = ::configureMavenDepsOnAnnotations)
+            }
+
+            ide {
+                acceptedLocations(ScriptAcceptedLocation.Everywhere)
+            }
+        }
+)
